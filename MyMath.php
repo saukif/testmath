d3a6ce333f9a7209ee5a815acd02d3e91e2b5022
saukif <saukif@yarikul.in>
2025-02-07 09:56:40 +0530

    add first file

---
+++ MyMath.php
@@ -0,0 +1,17 @@
+<?php
+namespace MyMathLib;
+
+class Math {
+    public function add($a, $b) {
+        return $a + $b;
+    }
+
+    public function sub($a, $b) {
+        return $a - $b;
+    }
+
+    public function multiply($a, $b) {
+        return $a * $b;
+    }
+}
+?>
